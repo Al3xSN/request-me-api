b18f41c05d1af8008c17bae357476008989952b4
anikolov <nikolovalex3@gmail.com>
2026-03-15 18:06:03 +0200

    Add Entities

---
+++ src/RequestMe/RequestMe.Repository/Entities/AuditedEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace RequestMe.Repository.Entities;
+
+public class AuditedEntity
+{
+    public Guid Id { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime UpdatedAt { get; set; }
+    
+    public string CreatedBy { get; set; }
+    
+    public string UpdatedBy { get; set; }
+
+    public bool IsActive { get; set; }
+}
---
+++ src/RequestMe/RequestMe.Repository/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace RequestMe.Repository.Entities;
+
+public class User : AuditedEntity
+{
+    public string Username { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string PhoneNumber { get; set; }
+
+    public IEnumerable<SongRequest> SongRequests { get; set; }
+}
